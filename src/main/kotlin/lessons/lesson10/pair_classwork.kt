3ba68f12897e4f1e67a7f1fe0df239bc14d5aab1
olgadorohova <o.panomareva@gmail.com>
2025-10-07 20:49:02 +0400

    lesson 10 + check homework lesson 09

--- src/main/kotlin/lessons/lesson10/pair_classwork.kt
+++ src/main/kotlin/lessons/lesson10/pair_classwork.kt
@@ -4,5 +4,9 @@ fun main () {
 
     val pair = 1 to "a"
 
+    val emptyMap: Map<String, Int> = mapOf( "adsada" to 1)
+
+    val capitals: Map<String, String>  = mapOf("Russia" to "Moscow", "France" to "Paris")
+    val map: Map<Int, String> = mapOf(1 to "a", 2 to "v", 3 to "f")
 
 }
