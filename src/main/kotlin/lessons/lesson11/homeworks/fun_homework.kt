d848b8c7b7405d49ca55072663ed5c2197adceb4
olgadorohova <o.panomareva@gmail.com>
2025-10-12 17:36:28 +0400

    homeworks

--- src/main/kotlin/lessons/lesson11/homeworks/fun_homework.kt
+++ src/main/kotlin/lessons/lesson11/homeworks/fun_homework.kt
@@ -1,5 +1,6 @@
 package ru.kotlin.lessons.lesson11.homeworks
 
+import ru.kotlin.homework.status
 import java.io.StringReader
 
 //Задачи на сигнатуру метода
@@ -43,4 +44,38 @@ fun a9(): List<String?> {
 //10 Принимает nullable строку и nullable целое число и возвращает nullable булево значение.
 fun a10 (a10: String?, a11: Int?): Boolean? {
     return false
+}
+
+// Задачи на написание кода
+//1 Напишите функцию multiplyByTwo, которая принимает целое число и возвращает его, умноженное на 2.
+fun multiplyByTwo (x1: Int): Int {
+    return x1 * 2
+}
+
+//2 Создайте функцию isEven, которая принимает целое число и возвращает true, если число чётное, и false в противном случае.
+fun isEven (x2: Int): Boolean {
+    return if (x2 % 2 == 0) {
+     true} else return false
+}
+//3 Напишите функцию printNumbersUntil, которая принимает целое число n и выводит на экран числа от 1 до n. Если число n меньше 1, функция должна прекратить выполнение с помощью return без вывода сообщений.
+fun printNumbersUntil (x3: Int) {
+    if (x3 < 1) return
+    for (i in 1..x3) {
+        println(i)
+    }
+}
+//4 Создайте функцию findFirstNegative, которая принимает список целых чисел и возвращает первое отрицательное число в списке. Если отрицательных чисел нет, функция должна вернуть null.
+fun findFirstNegative (x4: List<Int>): Int? {
+    for (num in x4) {
+        if (num < 0 ) return num
+    }
+    return null
+}
+//5 Напишите функцию processList, которая принимает список строк. Функция должна проходить по списку и выводить каждую строку. Если встречается null значение, функция должна прекратить выполнение с помощью return без возврата значения.
+fun processList (x5: List<String?>) {
+    for (string in x5) {
+        println(string)
+        if (string == null) return
+    }
+
 }
