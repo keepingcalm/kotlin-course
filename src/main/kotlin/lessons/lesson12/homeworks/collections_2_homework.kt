eea21e499dca6272d2c3d099bb398b2d703e7c00
olgadorohova <o.panomareva@gmail.com>
2025-10-16 19:23:58 +0400

    homework12

--- src/main/kotlin/lessons/lesson12/homeworks/collections_2_homework.kt
+++ src/main/kotlin/lessons/lesson12/homeworks/collections_2_homework.kt
@@ -3,7 +3,7 @@ package ru.kotlin.lessons.lesson12.homeworks
 fun main () {
 //Задачи на приведение коллекции к значению
 //1 Проверить, что размер коллекции больше 5 элементов.
-    val homework = listOf(-1, 5, 7, 8, 444, 88, 33)
+    val homework = listOf(-1, 5, 7, 19, 23, 8, 444, 88, 33)
     if (homework.size > 5 ) {
         println("yes")
     }
@@ -48,12 +48,34 @@ fun main () {
 
     //Задачи на обработку коллекций
 // 12 Отфильтровать коллекцию по диапазону 18-30
+    val res12 = homework.filter { it in 18..30 }
+    println(res12)
+
 // 13 Выбрать числа, которые не делятся на 2 и 3 одновременно
+    val res13 = homework.filter { it % 2 != 0 || it % 3 !=0 }
+    println(res13)
+
 // 14 Очистить текстовую коллекцию от null элементов
+    val homeworkText = listOf<String?>("ldsad", null, "sdsdsd", "eportpeot", "19", null, "asdad", "444", "88", "33xsada")
+    val res14 = homeworkText.filterNotNull()
+    println(res14)
+
 // 15 Преобразовать текстовую коллекцию в коллекцию длин слов
+    val res15 = homeworkText.map { it?.length ?: "nothing to count" }
+    println(res15)
+
 // 16 Преобразовать текстовую коллекцию в мапу, где ключи - перевёрнутые слова, а значения - длина слов
+    val res16 = homeworkText.map { it -> it?.reversed() to it?.length }
+    println(res16)
+
 // 17 Отсортировать список в алфавитном порядке
+    val res17 = homeworkText.sortedBy{it}
+    println(res17)
+
 // 18 Взять первые 3 элемента списка
+    val res18 = homeworkText.take(3)
+    println(res18)
+
 // 19 Распечатать квадраты элементов списка
 // 20 Группировать список по первой букве слов
 // 21 Очистить список от дублей
