1f3e57ed4fd8bf22d94cfb3c12387f629a679032
olgadorohova <o.panomareva@gmail.com>
2025-12-12 19:36:42 +0400

    lesson 26 class

---
+++ src/main/kotlin/lessons/lesson26/homeworks/homework_files.kt
@@ -0,0 +1,121 @@
+package ru.kotlin.lessons.lesson26.homeworks
+
+import ru.kotlin.lessons.lesson26.createFile
+import java.io.File
+
+
+fun main() {
+// Задача 1
+//Создайте текстовый файл workspace/task1/example.txt.
+// Запишите в него строку "Hello, Kotlin!", а затем проверьте, существует ли файл.
+// Реши задачу с использованием scope функции без создания переменной.
+
+
+    File("workspace/task1/example.txt").apply {
+        parentFile?.mkdirs()
+        writeText("Hello, Kotlin!")
+        println(if (exists()) "File exists" else "File doesn't exist")
+    }
+
+
+//Задача 2
+//Создайте директорию workspace/task2/testDir. Проверьте, является ли она директорией, и выведите её абсолютный путь.
+
+
+    File("workspace/task2/testDir").run {
+        mkdirs()
+        isDirectory
+        println(absolutePath)
+    }
+
+
+//Задача 3
+//Создайте директорию workspace/task3/structure.
+// Внутри директории structure создайте папку myDir с двумя вложенными поддиректориями subDir1 и subDir2.
+// Проверьте, что myDir действительно содержит эти поддиректории.
+// Используй scope функции для минимизации создания переменных а так же метод file.resolve("myDir")
+// для создания нового объекта File путём добавления к существующему пути ещё одной секции.
+// И так же для других директорий.
+
+    File("workspace/task3/structure").apply {
+        mkdirs()
+    }.resolve("myDir").apply {
+        listOf("subDir1", "subDir2").forEach { resolve(it).mkdirs() }
+    }.also { myDir ->
+        println("myDir path: ${myDir.absolutePath}")
+        println(
+            "subDir created: ${
+                listOf("subDir1", "subDir2").all {
+                    myDir.resolve(it).exists() && myDir.resolve(it).isDirectory
+                }
+            }"
+        )
+    }
+
+
+//Задача 4
+//Создайте директорию workspace/task4/temp.
+// Внутри директории temp создайте несколько вложенных файлов и директорий.
+// Удалите директорию workspace/task4 со всем содержимым
+
+    File("workspace/task4/temp").apply {
+        mkdirs()
+    }.also { ex4 ->
+        listOf("file1.txt", "file2.txt").forEach { name ->
+            File(ex4, name).createNewFile()
+        }
+        listOf("subDir1", "subDir2").forEach { name ->
+            File(ex4, name).mkdirs()
+        }
+    }.also { tempDir ->
+        File(tempDir, "subDir1/file3.txt").createNewFile()
+    }
+//    File("workspace/task4").run {
+//        deleteRecursively()
+//        println("Директория workspace/task4 удалена со всем содержимым")
+//    }
+
+//Задача 5
+//Создайте файл workspace/task5/config/config.txt. запишите в него список параметров (в формате ключ=значение), а затем прочитайте файл и выведите только значения.
+
+    File("workspace/task5/config/config.txt").apply {
+        parentFile?.mkdirs()
+        writeText(
+            mapOf(
+                "env" to "prod",
+                "bd" to "postgress",
+                "email_CTO" to "test@gmail.com"
+            ).toString()
+        )
+        println(readText())
+    }
+
+//Задача 6
+//Пройди по всем вложенным директориям workspace и выведи в консоль сначала пути директорий, а потом пути файлов
+
+    File("workspace").takeIf { it.exists() && it.isDirectory }?.run {
+        println("\nDirectories ex6:")
+        walk().filter { it.isDirectory }.forEach { println(it) }
+
+        println("\nFiles ex6:")
+        walk().filter { it.isFile }.forEach { println(it) }
+    } ?: println("dir workspace not found")
+
+//Задача 7
+//Создайте директорию workspace/task7/docs.
+// Проверь, есть ли файл с именем readme.md. Если файла нет, создайте его и запишите текст "This is a README file.".
+// Проверьте текст в файле.
+
+    File("workspace/task7/docs").resolve("readme.md").apply {
+        if (exists() && isFile) {
+            println(readText())
+        } else {
+            parentFile?.mkdirs()
+            createNewFile()
+            writeText("This is a README file.")
+            println(readText())
+        }
+    }
+}
+
+
